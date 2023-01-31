dbd34521d1237894e839eaa83499997c78cf3de8
Brent001Ito-san <brentito001@gmial.com>
2023-01-31 16:37:05 +0300

    PE: problem2

---
+++ Problem2/Main.java
@@ -0,0 +1,25 @@
+package Problem2;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = 0 ;
+        int sum = 0;
+        
+        // using while loop coz I don't know when the values will reach 4 million.
+        
+        while (a <= 4_000_000) {
+            
+            if (a % 2 ==0) 
+                sum += a;
+                
+        //System.out.print(a +  ", "); I only need a coz I'm swapping its value with the rest.
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        System.out.println(sum);
+    }
+    
+}
