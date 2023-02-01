d603b8454c77130fd020945644864b22dd5f7fca
Brent001Ito-san <brentito001@gmial.com>
2023-02-01 08:57:17 +0300

    Added so blogs about it ;-)

--- util/Prime.java
+++ util/Prime.java
@@ -1,14 +1,16 @@
 package util;
-
+//Created my own package to make it reusable.
 public class Prime {
     public static boolean isPrime(long x){
-
+        //2 and 3 are already known prime numbers so no need for calculating them.
         if (x == 2 || x == 3) {
             return true;
         }
-
+        // All even numbers can't be prime numbers.
         if(x % 2 == 0 || x == 1) return false;
 
+        //Checking potential odd numbers which might be prime numbers
+        //Using square root to make code more effecient deu to the associative law.
         for (int i = 3; i <= Math.sqrt(x) ; i+= 2) {
             if (x % i == 0) {
                 return false;
