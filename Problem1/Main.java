a3c0665ec768653bb03383632bf8eff36e199c2e
Brent001Ito-san <brentito001@gmial.com>
2023-02-01 12:41:54 +0300

    PE: Problem 1

--- Problem1/Main.java
+++ Problem1/Main.java
@@ -10,8 +10,8 @@ public class Main {
         int sum = 0;
 
         for (int i = 0; i < 1000; i++) {
-            //Checking if the number (i) is divisible by 3 or 5  
-            //Then perfom addition to all that is divisible
+            //Checking if the number (i) is divisible by 3 or 5. 
+            //Then perfom addition to all that is divisible.
             if ( i % 3 == 0 || i % 5 == 0 )
             {
                 sum +=i;
