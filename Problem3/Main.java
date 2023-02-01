cfc56d0213448a3370ea88802726070dfa816031
Brent001Ito-san <brentito001@gmial.com>
2023-02-01 12:42:20 +0300

    PE: Problem 3

--- Problem3/Main.java
+++ Problem3/Main.java
@@ -6,19 +6,19 @@ import util.Prime;
 public class Main {
     
     public static void main(String[] args) {
-        //Using long data type coz of the byte size required
+        //Using long data type coz of the byte size required.
         long num = 600851475143l;
 
         long prime = 2;
 
         for (int i = 2; num != 1; i++) {
-        //Checking if number is a prime number and swapping its value with 'i'
+        //Checking if number is a prime number and swapping its value with 'i'.
             if (Prime.isPrime(i)) {
                 prime = i;
             }else {
                 continue;
             }
-         //Dividing the number with all the prime numbers since we need its prime factors
+        //Dividing the number with all the prime numbers since we need its prime factors.
             while (num % prime == 0) {
                 num /= prime;
             }
