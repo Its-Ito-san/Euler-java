c3bab1a18ff5e487c9eaae46252aeafee6d835ec
Brent001Ito-san <brentito001@gmial.com>
2023-06-22 08:25:07 +0300

    Problem$_Palindorme Reverse

---
+++ Problem4/Main.java
@@ -0,0 +1,20 @@
+package Problem4;
+public class Main{
+    public static void main(String[] args) {
+        int largestPalindrome = 0;
+
+        for (int i = 100; i < 1000; i++) {
+        for (int j = i; j < 1000; j++) {
+        int product = i * j;
+        String productStr = Integer.toString(product);
+        if (productStr.equals(new StringBuilder(productStr).reverse().toString())) {
+            if (product > largestPalindrome) {
+                largestPalindrome = product;
+            }
+        }
+    }
+}
+
+System.out.println(largestPalindrome);
+    }
+}
