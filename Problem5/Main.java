b7127c3145f4a6652bb2d025e3f88ff50ad2d62f
Brent001Ito-san <brentito001@gmial.com>
2023-06-23 10:43:43 +0300

    Problem 5 ( Help me the code is inefficient)

---
+++ Problem5/Main.java
@@ -0,0 +1,29 @@
+package Problem5;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        float i;
+        float div;
+        
+        for(i=2520; i<=10000000; i++){
+
+            boolean hasReminder = false;
+
+            for(j=11; j<=20; j++){
+                
+                if(i%j !=0){
+                    hasReminder = true;
+                    break;
+                }
+            }
+
+            if(!hasReminder){
+
+                div = i;
+                break;
+            }
+        }
+        System.out.println(div);
+    }
+}
---
+++ Problem5/README.md
@@ -0,0 +1,10 @@
+# Smallest Multiple
+Problem 5
+---
+
+2520 is the smallest number that can be divided by each of the numbers from towithout any remainder.
+What is the smallest positive number that is evenly divisible by all of the numbers from to?
+    
+answer
+---
+ 232792560
